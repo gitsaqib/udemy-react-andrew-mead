0554b32213bb013460b8f051e039dcf5620fad7a
Saqib Butt <saqib.butt@globalnorthstar.com>
2017-07-23 23:55:11 +0500

    Heroku integration

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require("express");
 
+
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req,res,next){
+  if(req.headers['x-forwarded-proto'] === 'http' ){
+    next();
+  }else {
+      req.redirect('http://'+ req.hostname+req.url)
+  }
+});
 
 app.use(express.static("public"));
-
-app.listen(3000,function(){
-  console.log("Express server is running");
+app.listen(PORT,function(){
+  console.log("Express server is running"+PORT);
 });
