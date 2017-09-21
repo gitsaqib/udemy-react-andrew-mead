0876b2b754f6fd67a8c6d6207941f4c0b8758a8f
Saqib Butt <saqib.butt@globalnorthstar.com>
2017-09-22 00:30:26 +0500

    Redux in baby steps : step 3, connect() made dispatch() available in this.props

--- app/components/todo/TodoForm.jsx
+++ app/components/todo/TodoForm.jsx
@@ -1,16 +1,22 @@
 var React = require('react')
 var ReactDom = require('react-dom')
+var {connect} = require('react-redux');
 
 var TodoForm = React.createClass({
 
+  addTodo:function(){
+      var {dispatch} = this.props;
+      var text=this.refs.todo.value;
+      this.refs.todo.value = "";
+      dispatch({type:"ADD_TODO",text});
+  },  
   render : function(){
     return (
       <div className="container__footer">
         <input ref="todo" placeholder="Enter Todo" ></input>
-        <button className="button expanded"> Add </button>
+        <button className="button expanded" onClick={this.addTodo}> Add </button>
       </div>
     )
   }
 });
-
-module.exports = TodoForm;
+module.exports = connect()(TodoForm);
